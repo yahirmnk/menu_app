e76dd7a74345a9f7d6f799f4b1c7a2d9ab061c4f
Victor Yahir <corp.yahirmnk@gmail.com>
2025-08-13 02:28:28 -0600

    user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,25 +3,13 @@ class User {
   final String nombre;
   final String correo;
 
-  User({
-    required this.id,
-    required this.nombre,
-    required this.correo,
-  });
+  User({required this.id, required this.nombre, required this.correo});
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      id: json["_id"], // ✅ Ya viene como string
+      id: json["_id"]?.toString() ?? "",
       nombre: json["nombre"] ?? "",
       correo: json["correo"] ?? "",
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      "_id": id,
-      "nombre": nombre,
-      "correo": correo,
-    };
   }
 }
