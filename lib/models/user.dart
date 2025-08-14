04e7a496bcf3821d741f6aee272379f2fe90def3
Victor Yahir <corp.yahirmnk@gmail.com>
2025-08-13 23:21:34 -0600

    catualizacion en user

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -6,8 +6,19 @@ class User {
   User({required this.id, required this.nombre, required this.correo});
 
   factory User.fromJson(Map<String, dynamic> json) {
+    String id = "";
+
+  // Si _id es un string directamente
+    if (json["_id"] is String) {
+      id = json["_id"];
+    }
+  // Si _id es un objeto tipo {"$oid": "..."}
+    else if (json["_id"] is Map && json["_id"]["\$oid"] is String) {
+      id = json["_id"]["\$oid"];
+    }
+
     return User(
-      id: json["_id"]?.toString() ?? "",
+      id: id,
       nombre: json["nombre"] ?? "",
       correo: json["correo"] ?? "",
     );
